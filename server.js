87091e2e609769c6ea7203c88d4a5fd0905942c0
StarkOne <stark.pro40@gmail.com>
2020-06-16 23:12:21 +0700

    models add

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //const logger = require('./middleware/logger');
 
@@ -11,6 +13,9 @@ dotenv.config({
   path: './config/config.env',
 });
 
+//connect DB
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -18,8 +23,18 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/v1/bootcamps', routerBootcamps);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`.yellow.bold);
+});
+
+//handle unhandled promise
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error ${err.message}`.red);
+  server.close(() => process.exit(1));
 });
